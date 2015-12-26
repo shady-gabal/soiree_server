8e28387888c191bf9807d43b638bb1611260fd5a
Shady Gabal <shady@nyu.edu>
2015-12-26 18:56:32 -0500

    passport

--- app.js
+++ app.js
@@ -55,8 +55,10 @@ passport.use(new facebookTokenStrategy({
         //console.log(profile);
 
         User.findByFacebookUserId(profile.id, function (user) {
+            console.log("passport use - found user");
             return done(null, user);
         }, function(err){
+            console.log("passport use - did not find user");
             return done(err, null);
         });
 
