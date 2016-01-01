a3f35e2131d86826fedf8467b8a02289f67f3f19
Shady Gabal <shady@nyu.edu>
2015-12-31 19:50:48 -0500

    m

--- routes/api/users.js
+++ routes/api/users.js
@@ -59,7 +59,7 @@ router.post('/findUser', function(req, res, next){
         return ResHelpers.sendMessage(res, 404, "Error fetching user specified");
       }
       else if (!user){
-        sendUser(res, null);
+        sendUser(res, user);
       }
       else{
         var deviceUUID = req.body.deviceUUID;
