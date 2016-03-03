b25697e79d671269a0ceffc6db5729a971498634
Shady Gabal <shady@nyu.edu>
2016-03-03 15:38:10 -0500

    socket

--- routes/api/soireeInProgress.js
+++ routes/api/soireeInProgress.js
@@ -25,25 +25,25 @@ var returnRouter = function(io) {
 
     var _socket;
 
+    io.on('connection', function(socket){
+        //_socket = socket;
+        console.log('a user connected to soireeInProgress');
+
+        //socket.on('event name', function(data){});
+
+        var message = {author: SOIREE, text : "Connected to " + SOIREE_LOWERCASE};
+        socket.emit('test', message);
+        //console.log("sent message");
+
+        //res.json({"status" : "Connected"});
+
+        socket.on('disconnect', function(){
+            console.log('user disconnected from soireeInProgress');
+        });
+    });
+
     router.get('/', function (req, res) {
         console.log("/soireeInProgress requested");
-        io.on('connection', function(socket){
-            _socket = socket;
-
-            console.log('a user connected to soireeInProgress');
-
-            //socket.on('event name', function(data){});
-
-            var message = {author: SOIREE, text : "Connected to " + SOIREE_LOWERCASE};
-            //socket.emit('test', message);
-            //console.log("sent message");
-
-            //res.json({"status" : "Connected"});
-
-            socket.on('disconnect', function(){
-                console.log('user disconnected from soireeInProgress');
-            });
-        });
         //res.render('index', {title: 'Express'});
     });
 
