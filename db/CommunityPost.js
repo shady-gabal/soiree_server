c1245484ae4c8d4496d3d9d9da72219dc71532c2
Shady Gabal <shady@nyu.edu>
2016-03-28 13:11:15 -0400

    notifications

--- db/CommunityPost.js
+++ db/CommunityPost.js
@@ -331,7 +331,7 @@ postSchema.methods.jsonObject = function(user, showComments){
 };
 
 postSchema.methods.userAddedComment = function(user, comment){
-    console.log("addedComment()");
+    console.log("userAddedComment() with " + user);
     Notification.createCommentedOnPostNotifications(user, this, comment);
     //if (!this.populated("_user")){
     //    this.deepPopulate("_user", function(err, post){
