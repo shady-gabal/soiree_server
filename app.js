0de0cb59ec77acde40f2c4b7c68a47f56348e052
Shady Gabal <shady@nyu.edu>
2016-06-03 02:45:44 -0400

    soireecreator

--- app.js
+++ app.js
@@ -355,7 +355,7 @@ function scheduleCron(){
         //new CronJob(cronExpression, function(){console.log("Cron job being run...");}, null, true, 'America/New_York');
         //new CronJob(cronExpression, function(){console.log("Cron job being run...");}, null, true, 'America/New_York');
         new CronJob('0 0-59/10 * * * *', soireeStarterStopper, null, true, 'America/New_York');
-        new CronJob('0 1 12 * * *', soireeCreator, null, true, 'America/New_York');
+        new CronJob('0 47 2 * * *', soireeCreator, null, true, 'America/New_York');
 
         //new CronJob('0 0-59/5 * * * *', soireeCreator, null, true, 'America/New_York');
 
