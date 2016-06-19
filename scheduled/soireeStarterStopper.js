6bf591bd6097dbfb28831c6bd2c9e345fd60eb04
Shady Gabal <shady@nyu.edu>
2016-06-18 22:46:40 -0400

    fixed memory leak

--- scheduled/soireeStarterStopper.js
+++ scheduled/soireeStarterStopper.js
@@ -23,83 +23,79 @@ var scheduledTasks = function(){
     console.log("Running scheduled soirees task for scheduledTimeIdentifierReminder: " + scheduledTimeIdentifierReminder +  " ...");
     //
     ////START OR CANCEL IF NECESSARY
-    //console.log('starting soirees starting...');
-    //
-    //Soiree.find( { "scheduledStartTimeIdentifier" : {"$lte" : scheduledTimeIdentifierNow}, "started" : false, "ended" : false, "cancelled" : false} ).deepPopulate(deepPopulateFields).exec(function(err, soirees){
-    //    if (err){
-    //        console.log("Error in scheduledSoirees: " + err);
-    //    }
-    //    else{
-    //        console.log("Starting " + soirees.length + " soirees");
-    //        for (var i = 0; i < soirees.length; i++){
-    //            var soiree = soirees[i];
-    //            if (soiree.reachedNumUsersMin){
-    //                soiree.open();
-    //                //console.log("Starting soiree  " + soiree.soireeId + " with users attending: " + soiree.numUsersAttending);
-    //                soiree.startIfPossible();
-    //            }
-    //            else{
-    //                soiree.cancel();
-    //            }
-    //
-    //
-    //        }
-    //        console.log('done starting');
-    //    }
-    //});
+    console.log('starting soirees starting...');
+
+    Soiree.find( { "scheduledStartTimeIdentifier" : {"$lte" : scheduledTimeIdentifierNow}, "started" : false, "ended" : false, "cancelled" : false} ).deepPopulate(deepPopulateFields).exec(function(err, soirees){
+        if (err){
+            console.log("Error in scheduledSoirees: " + err);
+        }
+        else{
+            console.log("Starting " + soirees.length + " soirees");
+            for (var i = 0; i < soirees.length; i++){
+                var soiree = soirees[i];
+                if (soiree.reachedNumUsersMin){
+                    soiree.open();
+                    //console.log("Starting soiree  " + soiree.soireeId + " with users attending: " + soiree.numUsersAttending);
+                    soiree.startIfPossible();
+                }
+                else{
+                    soiree.cancel();
+                }
+
+
+            }
+            console.log('done starting');
+        }
+    });
 
 //    END
 //end existing soirees
-//    console.log('ending soirees starting...');
-//    Soiree.find( { "scheduledEndTimeIdentifier" : {"$lte" : scheduledTimeIdentifierNow}, "started" : true, "ended" : false, "cancelled" : false} ).populate(deepPopulateFields).exec(function(err, soirees){
-        //if (err){
-        //    console.log("Error in scheduledSoirees: " + err);
-        //}
-        //else{
-        //    console.log("Ending " + soirees.length + " soirees");
-        //    for (var i = 0; i < soirees.length; i++){
-        //        var soiree = soirees[i];
-        //        console.log('trying soiree ' + soiree.soireeId);
-        //
-        //        if (soiree.soireeType != "TEST"){
-        //            soiree.end();
-        //        }
-        //    }
-        //    console.log('done ending');
-        //
-        //}
-    //});
+    Soiree.find( { "scheduledEndTimeIdentifier" : {"$lte" : scheduledTimeIdentifierNow}, "started" : true, "ended" : false, "cancelled" : false} ).populate(deepPopulateFields).exec(function(err, soirees){
+        if (err){
+            console.log("Error in scheduledSoirees: " + err);
+        }
+        else{
+            console.log("Ending " + soirees.length + " soirees");
+            for (var i = 0; i < soirees.length; i++){
+                var soiree = soirees[i];
+                console.log('trying soiree ' + soiree.soireeId);
+
+                if (soiree.soireeType != "TEST"){
+                    soiree.end();
+                }
+            }
+            console.log('done ending');
+
+        }
+    });
 
     console.log('ending soirees starting...');
     Soiree.find( { } ).populate(deepPopulateFields).exec(function(err, soirees){
 
     });
-    console.log("done ending");
 
-//    //REMIND
-////remind people of upcoming soirees or cancel if necessary
-//    Soiree.find( { "scheduledStartTimeIdentifier" : {"$lte" : scheduledTimeIdentifierReminder}, "started" : false, "ended" : false, "cancelled" : false} ).deepPopulate(deepPopulateFields).exec(function(err, soirees){
-//        if (err){
-//            console.log("Error in scheduledSoirees: " + err);
-//        }
-//        else{
-//            console.log("Reminding users of " + soirees.length + " soirees");
-//            for (var i = 0; i < soirees.length; i++){
-//                var soiree = soirees[i];
-//                //console.log("Reminding soiree " + soiree.soireeId + " with users attending: " + soiree.numUsersAttending);
-//                if (soiree.reachedNumUsersMin){
-//                    soiree.open();
-//                    soiree.remind(SOIREE_REMIND_BEFORE + "");
-//                }
-//                else{
-//                    soiree.cancel();
-//                }
-//            }
-//            console.log('done reminding');
-//        }
-//    });
+    //REMIND
+//remind people of upcoming soirees or cancel if necessary
+    Soiree.find( { "scheduledStartTimeIdentifier" : {"$lte" : scheduledTimeIdentifierReminder}, "started" : false, "ended" : false, "cancelled" : false} ).deepPopulate(deepPopulateFields).exec(function(err, soirees){
+        if (err){
+            console.log("Error in scheduledSoirees: " + err);
+        }
+        else{
+            console.log("Reminding users of " + soirees.length + " soirees");
+            for (var i = 0; i < soirees.length; i++){
+                var soiree = soirees[i];
+                //console.log("Reminding soiree " + soiree.soireeId + " with users attending: " + soiree.numUsersAttending);
+                if (soiree.reachedNumUsersMin){
+                    soiree.open();
+                    soiree.remind(SOIREE_REMIND_BEFORE + "");
+                }
+                else{
+                    soiree.cancel();
+                }
+            }
+        }
+    });
 //
-    console.log('done running');
 };
 
 module.exports = scheduledTasks;
