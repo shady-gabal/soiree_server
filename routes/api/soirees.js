d61563188387b44eee889b49ce1b000db5da16ac
Shady Gabal <shady@nyu.edu>
2016-06-22 17:36:07 -0400

    fixed soiree joining for braintree

--- routes/api/soirees.js
+++ routes/api/soirees.js
@@ -91,7 +91,7 @@ router.get('/soireesNear', function (req, res) {
 router.post('/joinSoiree', function (req, res, next) {
     User.verifyUser(req, res, next, function (user) {
         if (!user.chargeable) {
-            return ResHelper.sendError(res, ErrorCodes.MissingStripeCustomerId);
+            return ResHelper.sendError(res, ErrorCodes.InvalidInput);
         }
 
         var soireeId = req.body.soireeId;
