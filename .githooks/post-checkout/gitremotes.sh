981e9350f4bfaecd1b90d8f3f2fddc731e221d0f
Shady Gabal <shady@nyu.edu>
2016-07-13 13:51:35 -0400

    git hooks

---
+++ .githooks/post-checkout/gitremotes.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git remote rm origin &> ../log
+git remote rm development &> ../log
+git remote rm production &> ../log
+git remote add origin https://github.com/experience-soiree/soiree_server.git &> ../log
+git remote add development https://git.heroku.com/soireeapp-development.git &> ../log
+git remote add production https://git.heroku.com/soireeapp.git &> ../log
