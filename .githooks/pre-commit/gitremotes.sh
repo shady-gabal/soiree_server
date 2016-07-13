0fa956560b4e5db64d8f005e15dc0211a0be7476
Shady Gabal <shady@nyu.edu>
2016-07-13 17:13:26 -0400

    git hooks

--- .githooks/pre-commit/gitremotes.sh
+++ .githooks/pre-commit/gitremotes.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-git remote rm origin &> ../log
-git remote rm development &> ../log
-git remote rm production &> ../log
-git remote add development https://git.heroku.com/soireeapp-development.git &> ../log
-git remote add production https://git.heroku.com/soireeapp.git &> ../log
-git remote add origin https://github.com/experience-soiree/soiree_server.git &> ../log
+git remote rm origin &> /dev/null
+git remote rm development &> /dev/null
+git remote rm production &> /dev/null
+git remote add development https://git.heroku.com/soireeapp-development.git &> /dev/null
+git remote add production https://git.heroku.com/soireeapp.git &> /dev/null
+git remote add origin https://github.com/experience-soiree/soiree_server.git &> /dev/null
